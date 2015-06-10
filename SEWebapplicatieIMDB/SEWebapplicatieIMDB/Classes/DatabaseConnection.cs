0d31d32733488af93dada7c3440b26207d0fb096
nickbijmoer <nick@bijmoer.nl>
2015-06-10 23:45:01 +0200

    Adding Databaseconnection

---
+++ SEWebapplicatieIMDB/SEWebapplicatieIMDB/Classes/DatabaseConnection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEWebapplicatieIMDB.Classes
+{
+    using Oracle.DataAccess.Client;
+    public class DatabaseConnection
+    {
+
+        protected OracleConnection connection = new OracleConnection();
+        protected string connectionString = "DATA SOURCE=//192.168.15.50:1521/fhictora;PASSWORD=K9k8zLNCO0;USER ID=dbi292421";
+
+
+        public DatabaseConnection()
+        {
+
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                connection = new OracleConnection();
+                connection.ConnectionString = connectionString;
+                connection.Open();
+            }
+            catch
+            {
+                connection.Close();
+            }
+        }
+
+        public void DisConnect()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+    
+
+}
---
+++ SEWebapplicatieIMDB/SEWebapplicatieIMDB/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEWebapplicatieIMDB.Startup))]
+namespace SEWebapplicatieIMDB
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

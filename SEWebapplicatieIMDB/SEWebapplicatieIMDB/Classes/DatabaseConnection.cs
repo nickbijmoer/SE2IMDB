d8bcd5b3600e3db7976120c1d2d2e8d351875005
nickbijmoer <nick@bijmoer.nl>
2015-06-11 00:12:12 +0200

    Still working on dbconnection

--- SEWebapplicatieIMDB/SEWebapplicatieIMDB/Classes/DatabaseConnection.cs
+++ SEWebapplicatieIMDB/SEWebapplicatieIMDB/Classes/DatabaseConnection.cs
@@ -10,7 +10,7 @@ namespace SEWebapplicatieIMDB.Classes
     {
 
         protected OracleConnection connection = new OracleConnection();
-        protected string connectionString = "DATA SOURCE=//192.168.15.50:1521/fhictora;PASSWORD=K9k8zLNCO0;USER ID=dbi292421";
+        protected string connectionString = "DATA SOURCE=//192.168.15.50:1521/fhictora;PASSWORD=ZaozMNrus2;USER ID=dbi324529";
 
 
         public DatabaseConnection()

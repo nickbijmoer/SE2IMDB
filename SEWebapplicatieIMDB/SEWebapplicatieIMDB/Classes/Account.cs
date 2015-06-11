497082f3d661b84ad10413287fcce56bf66080d4
nickbijmoer <nick@bijmoer.nl>
2015-06-11 15:52:01 +0200

    Account Class aangemaakt en Insert voor account gemaakt

---
+++ SEWebapplicatieIMDB/SEWebapplicatieIMDB/Classes/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEWebapplicatieIMDB.Classes
+{
+    public class Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime YearOfBirth { get; set; }
+        public string Country { get; set; }
+        public String PostalCode { get; set; }
+        public string EMail { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Account(string firstname, string lastname, string gender, DateTime yearofbirth, string country, string postalcode, string email, string username, string password)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Gender = gender;
+            YearOfBirth = yearofbirth;
+            Country = country;
+            PostalCode = postalcode;
+            EMail = email;
+            UserName = username;
+            Password = password;
+        }
+    }
+}
--- SEWebapplicatieIMDB/SEWebapplicatieIMDB/Classes/DBAccount.cs
+++ SEWebapplicatieIMDB/SEWebapplicatieIMDB/Classes/DBAccount.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Oracle.DataAccess.Client;
 
 namespace SEWebapplicatieIMDB.Classes
 {
-    public class DBAccount
+    public class DBAccount :DatabaseConnection
     {
+        public bool Insert(Account account)
+        {
+            bool ishetgelukt = false;
+
+            string sql = "INSERT INTO Account(Firstname,Lastname,Gender,Yearofbirth,Country,Postalcode,Email,Rol,Username,Password) VALUES (:FirstName, :LastName, :Gender, :YearOfBirth, :Country, :PostalCode, :Email, :Rol, :UserName, :Password)";
+            try
+            {
+
+                this.Connect();
+                OracleCommand cmd = new OracleCommand(sql, this.connection);
+                cmd.Parameters.Add(new OracleParameter("FirstName", account.FirstName));
+                cmd.Parameters.Add(new OracleParameter("LastName",account.LastName));
+                cmd.Parameters.Add(new OracleParameter("Gender",account.Gender));
+                cmd.Parameters.Add(new OracleParameter("YearOfBirth",account.YearOfBirth));
+                cmd.Parameters.Add(new OracleParameter("Country",account.Country));
+                cmd.Parameters.Add(new OracleParameter("PostalCode", account.PostalCode));
+                cmd.Parameters.Add(new OracleParameter("Email", account.EMail));
+                cmd.Parameters.Add(new OracleParameter("Rol","Geregistreerd" ));
+                cmd.Parameters.Add(new OracleParameter("UserName", account.UserName));
+                cmd.Parameters.Add(new OracleParameter("Password", account.Password));
+
+                ishetgelukt = true;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+
+            return ishetgelukt;
+        }
     }
 }
